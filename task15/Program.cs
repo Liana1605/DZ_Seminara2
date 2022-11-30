ccd9240f9a119c25c1ab31e163dfcb8b9e552114
liana16 <liana_you@bk.ru>
2022-11-30 15:42:37 +0300

    Задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите програму, которая принимает на вход цифру, 
+// обозначающую день недели и проверяет, является ли это день выходным
+
+Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (0 < Num && Num < 8)
+{
+    if (Num == 6 || Num == 7)
+    Console.WriteLine("Да");
+    else
+    Console.WriteLine("Нет");
+}
